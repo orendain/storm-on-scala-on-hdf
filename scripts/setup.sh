ecdc138cfdcc7e60f881213df1bf818404c20e1e
Edgar Orendain <edgar@orendainx.com>
2017-05-15 14:13:27 -0700

    Debugging

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,14 +13,14 @@ projectDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 cd $projectDir
 
 
-echo "Setting delete.topic.enable to true via Ambari"
-/var/lib/ambari-server/resources/scripts/configs.py $ambariUser $ambariPass 8080 http set $hostname $ambariClusterName kafka-broker delete.topic.enable true
+#echo "Setting delete.topic.enable to true via Ambari"
+#/var/lib/ambari-server/resources/scripts/configs.py $ambariUser $ambariPass 8080 http set $hostname $ambariClusterName kafka-broker delete.topic.enable true
 
 
-echo "Restarting Kafka via Ambari"
-curl -u $ambariUser:$ambariPass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Stop Kafka"}, "ServiceInfo": {"state": "INSTALLED"}}' http://$hostname:8080/api/v1/clusters/$ambariClusterName/services/KAFKA
-sleep 10
-curl -u $ambariUser:$ambariPass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Start Kafka"}, "ServiceInfo": {"state": "STARTED"}}' http://$hostname:8080/api/v1/clusters/$ambariClusterName/services/KAFKA
+#echo "Restarting Kafka via Ambari"
+#curl -u $ambariUser:$ambariPass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Stop Kafka"}, "ServiceInfo": {"state": "INSTALLED"}}' http://$hostname:8080/api/v1/clusters/$ambariClusterName/services/KAFKA
+#sleep 10
+#curl -u $ambariUser:$ambariPass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Start Kafka"}, "ServiceInfo": {"state": "STARTED"}}' http://$hostname:8080/api/v1/clusters/$ambariClusterName/services/KAFKA
 
 
 echo "Checking for SBT, installing if missing"
@@ -28,13 +28,13 @@ curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm
 yum -y install sbt-0.13.13.1-1
 
 
-echo "Creating Kafka topics"
-scripts/create-kafka-topics.sh
+#echo "Creating Kafka topics"
+#scripts/create-kafka-topics.sh
 
 
 echo "Extracting included web application"
 tar zxvf trucking-web-application-backend-0.4.0-SNAPSHOT.tgz
 
 
-echo "Building Storm topology"
-scripts/build-topology.sh
+#echo "Building Storm topology"
+#scripts/build-topology.sh
