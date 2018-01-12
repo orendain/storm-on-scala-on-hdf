47bdce89f2a4863117a09e9a8b6bb9747b3b2408
Edgar Orendain <edgar@orendainx.com>
2018-01-11 20:14:04 -0800

    Add option for deploying prebuilt topology or rebuilding+deploying

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -2,4 +2,5 @@
 
 scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+# Run trucking simulator, logging data to two different Kafka topics (trucking_data_truck and trucking_data_traffic)
 java -jar $scriptDir/trucking-simulator-assembly-0.5.1.jar com.orendainx.trucking.simulator.simulators.EnrichToKafkaSimulator
