0207ba2256ba6c5f41de2ab2d414ae2e91e64e3e
Edgar Orendain <edgar@orendainx.com>
2018-03-09 12:37:59 -0800

    Script changes

--- scripts/deploy-prebuilt-topology.sh
+++ scripts/deploy-prebuilt-topology.sh
@@ -3,4 +3,4 @@
 scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Deploy pre-built topology
-storm jar scriptDir/trucking-iot-demo-storm-on-scala-assembly-1.1.0.jar com.orendainx.trucking.storm.topologies.KafkaToKafka
+storm jar $scriptDir/trucking-iot-demo-storm-on-scala-assembly-1.1.0.jar com.orendainx.trucking.storm.topologies.KafkaToKafka
--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -3,4 +3,4 @@
 scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Run trucking simulator, logging data to two different Kafka topics (trucking_data_truck and trucking_data_traffic)
-java -jar $scriptDir/trucking-simulator-assembly-0.5.1.jar com.orendainx.trucking.simulator.simulators.EnrichToKafkaSimulator
+java -jar $scriptDir/trucking-simulator-assembly-0.5.2.jar com.orendainx.trucking.simulator.simulators.EnrichToKafkaSimulator
