c64e9d15d58d274c2d9ba9940b17151c23c5f1b0
Edgar Orendain <edgar@orendainx.com>
2018-03-13 19:08:00 -0700

    Simulator run script changes

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -3,4 +3,5 @@
 scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Run trucking simulator, logging data to two different Kafka topics (trucking_data_truck and trucking_data_traffic)
-java -jar $scriptDir/trucking-simulator-assembly-0.5.2.jar com.orendainx.trucking.simulator.simulators.EnrichToKafkaSimulator
+# By default, this is set up to run the main function from the class com.orendainx.trucking.simulator.simulators.EnrichToKafkaSimulator
+java -jar $scriptDir/trucking-simulator-assembly-0.5.2.jar
