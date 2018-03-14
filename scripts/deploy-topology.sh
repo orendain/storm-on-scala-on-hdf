1cb06ebe76f4bc101ed14672c3a80684e24f1f4b
Edgar Orendain <edgar@orendainx.com>
2018-03-13 17:28:55 -0700

    Fix path in storm deploy script

--- scripts/deploy-topology.sh
+++ scripts/deploy-topology.sh
@@ -2,4 +2,4 @@
 
 projectDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 
-storm jar $projectDir/trucking-storm-topology/target/trucking-storm-topology-0.3.2.jar com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafka
+storm jar $projectDir/trucking-storm-topology/target/scala-2.11/trucking-storm-topology-0.3.2.jar com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafka
